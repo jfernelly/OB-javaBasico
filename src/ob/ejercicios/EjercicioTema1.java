0a278dbd7b5c8d01ab874ebe1aa14026fbafd453
josefernelly <jfernelly@gmail.com>
2022-08-09 04:46:49 -0500

    Entrega ejercicios tema_1

---
+++ src/ob/ejercicios/EjercicioTema1.java
@@ -0,0 +1,20 @@
+package ob.ejercicios;
+
+public class EjercicioTema1 {
+    public static void main(String[] args) {
+        int edad = 33;
+        long cantidad = 33;
+        double estimado = 33.3;
+        boolean validado = true;
+        String saludo = "Hola mundo!!";
+
+        System.out.println(
+                        "Variables: {Edad->"        +edad+", "+
+                        "Cantidad->"    +cantidad+", "+
+                        "Estimado->"    +estimado+", "+
+                        "Validado->"    +validado+", "+
+                        "Saludo->"      +saludo+" }"
+        );
+
+    }
+}
