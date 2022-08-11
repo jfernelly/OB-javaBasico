f0c1d9f736fef5f165e16a3d343493435444e841
josefernelly <jfernelly@gmail.com>
2022-08-11 07:51:55 -0500

    Ejercicio_2

---
+++ src/ob/ejercicios/EjercicioTema2.java
@@ -0,0 +1,14 @@
+package ob.ejercicios;
+
+public class EjercicioTema2 {
+    public static void main(String[] args) {
+
+        int precioIVA = precioIVA(400);
+
+    }
+
+    static private int precioIVA(int precio){
+        return precio *= 1.1;
+    }
+
+}
